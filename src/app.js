54fc1f033db7c2fb4e1a0ec96440fec156b1fd57
Wesley Sanches <wesleysanches27@outlook.com>
2025-01-26 13:43:48 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+
+// rotas
+const movieRoutes = require('./routes/movies');
+const uerRoutes = require('./routes/users');
+app.use('/movies', movieRoutes);
+app.use('/users', userRoutes);
+
+// conexão com banco de dados
+mongoose.connect(process.env.MONGO_URI, )
+
+
+
+
