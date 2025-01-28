ee7f4c3e754117a398fe91e9e0ad4eb85509facc
Wesley Sanches <wesleysanches27@outlook.com>
2025-01-27 22:24:34 -0300

    fix app.js

--- src/app.js
+++ src/app.js
@@ -1,18 +1,49 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const cors = require('cors');
+const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+// sequelize para conectar com o banco de dados
+const sequelize = new Sequelize(
+  process.env.DB_NAME,    // nome do banco de dados
+  process.env.DB_USER,    // usuario do banco de dados
+  process.env.DB_PASSWORD, // senha do banco de dados
+  {
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    port: process.env.DB_PORT || 3306
+  }
+);
+
+// testar a conexao com o bando de dados
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Conexão com o banco de dados estabelecida com sucesso!');
+  } catch (error) {
+    console.error('Erro ao conectar ao banco de dados:', error);
+  }
+})();
 
 // rotas
 const movieRoutes = require('./routes/movies');
-const uerRoutes = require('./routes/users');
+const userRoutes = require('./routes/users');
+const categoryRoutes = require('./routes/category');
+
 app.use('/movies', movieRoutes);
 app.use('/users', userRoutes);
+app.use('/categories', userCategories);
 
-// conexão com banco de dados
-mongoose.connect(process.env.MONGO_URI, )
+// inicia o servidor
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
+
+
+
+
